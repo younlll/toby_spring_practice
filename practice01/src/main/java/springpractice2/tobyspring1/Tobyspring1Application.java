05de6a1221bb17dfae196a8059008d2d5c6489f5
younlll <youn.lee96@gmail.com>
2021-12-21 06:44:50 +0900

    Add files via upload

---
+++ practice01/src/main/java/springpractice2/tobyspring1/Tobyspring1Application.java
@@ -0,0 +1,13 @@
+package springpractice2.tobyspring1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tobyspring1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tobyspring1Application.class, args);
+	}
+
+}
---
+++ practice01/src/test/java/springpractice2/tobyspring1/Tobyspring1ApplicationTests.java
@@ -0,0 +1,13 @@
+package springpractice2.tobyspring1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tobyspring1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
