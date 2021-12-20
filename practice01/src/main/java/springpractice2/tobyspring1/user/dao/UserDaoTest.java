2c4b2dd79d0a8cc66c366b573f17314ca5e67866
이유나 <youn.lee96@gmail.com>
2021-12-21 06:59:50 +0900

    modify: Spring bean factory application

--- practice01/src/main/java/springpractice2/tobyspring1/user/dao/UserDaoTest.java
+++ practice01/src/main/java/springpractice2/tobyspring1/user/dao/UserDaoTest.java
@@ -1,12 +1,15 @@
 package springpractice2.tobyspring1.user.dao;
 
-import spring.user.domain.UserDao;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import springpractice2.tobyspring1.user.domain.UserDao;
 
 import java.sql.SQLException;
 
 public class UserDaoTest {
     public static void main(String[] args) throws ClassNotFoundException, SQLException {
-        ConnectionMaker connectionMaker = new DConnectionMaker();
-        UserDao dao = new UserDao(connectionMaker);
+        ApplicationContext context =
+                new AnnotationConfigApplicationContext(DaoFactory.class);
+        UserDao dao = context.getBean("userDao", UserDao.class);
     }
 }
