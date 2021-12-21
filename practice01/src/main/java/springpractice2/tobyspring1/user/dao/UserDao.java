c659245f217e7f9730a43a838ff069914dfba1cf
이유나 <youn.lee96@gmail.com>
2021-12-21 22:33:58 +0900

    add: create xml

---
+++ practice01/src/main/java/springpractice2/tobyspring1/user/dao/UserDao.java
@@ -0,0 +1,54 @@
+package springpractice2.tobyspring1.user.dao;
+
+import springpractice2.tobyspring1.user.domain.User;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class UserDao {
+    private ConnectionMaker connectionMaker;
+    private Connection conn;
+    private User user;
+
+    public UserDao(ConnectionMaker connectionMaker) {
+        this.connectionMaker = connectionMaker;
+    }
+
+    public void add(User user) throws ClassNotFoundException, SQLException {
+        this.conn = connectionMaker.makeConnection();
+
+        PreparedStatement ps = conn.prepareStatement(
+                "insert into users(id, name, password) values (?, ?, ?)");
+        ps.setString(1, user.getId());
+        ps.setString(2, user.getName());
+        ps.setString(3, user.getPassword());
+
+        ps.executeUpdate();
+
+        ps.close();
+        conn.close();
+    }
+
+    public User get(String id) throws ClassNotFoundException, SQLException {
+        this.conn = connectionMaker.makeConnection();
+
+        PreparedStatement ps = conn.prepareStatement(
+                "select * from users where id = ?");
+        ps.setString(1, id);
+
+        ResultSet rs = ps.executeQuery();
+        rs.next();
+        this.user = new User();
+        this.user.setId(rs.getString("id"));
+        this.user.setName(rs.getString("name"));
+        this.user.setPassword(rs.getString("password"));
+
+        rs.close();
+        ps.close();
+        this.conn.close();
+
+        return this.user;
+    }
+}
